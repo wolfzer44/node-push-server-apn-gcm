6c91ae8b9a0d0e385fe749357537d95d10524ea0
Matheus Oliveira <mattoliveirabsb@gmail.com>
2016-06-15 12:04:57 -0300

    added cors and middleware to origin server validation

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/index');
 const midds = require('./middlewares/index');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/api/v1/', midds.PushRouter, routes.Api);
+app.use('/api/v1/', cors(), midds.Origin, midds.PushRouter, routes.Api);
 
 app.listen(9000, function() {
   console.log('listend in: 9000');

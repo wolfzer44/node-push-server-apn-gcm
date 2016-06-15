9656c03a3ae5bf351317551d7e61b2457aa69275
Matheus Oliveira <mattoliveirabsb@gmail.com>
2016-06-15 12:25:47 -0300

    add support to ssl

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const routes = require('./routes/index');
 const midds = require('./middlewares/index');
+
+//SET ENVIRIOMENT
+process.env.NODE_ENV = 'development';
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/api/v1/', cors(), midds.Origin, midds.PushRouter, routes.Api);
 
-app.listen(9000, function() {
-  console.log('listend in: 9000');
-});
+// console.log(env)
+
+const cert = fs.readFileSync('./ssl/cert.crt', 'utf8');
+const key = fs.readFileSync('./ssl/key.key', 'utf8');
+
+const ssl = {cert: cert, key: key};
+const port = 9000;
+
+if(process.env.NODE_ENV === 'development') {
+  app.listen(9000, function() {
+    console.log('listend in: ' + port);
+  });
+}
+
+if(process.env.NODE_ENV === 'production') {
+  const httpsServer = https.createServer(ssl, app);
+  httpsServer.listen(port);
+}
